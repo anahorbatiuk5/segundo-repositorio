da8f1374dc636784c7e29e28922cc1852b7640fe
anahorbatiuk5 <ana.vitoriahorbatiuk.ramos@escola.pr.gov.br>
2024-11-13 19:54:44 -0300

    aula dia 13/11

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
-function criaCartao (item, pergunta, resposta) {
-    //console.log(item, pergunta,resposta)
 
+function criaCartao (item, pergunta, resposta) {
     let flashcard = document.getElementById('flashcard');
     let cartao = document.createElement('article');
-
     cartao.className='cartao';
 
     cartao.innerHTML=`
         <div class="cartao_conteudo"
-                <h3>Missão</h3>
-
-                <div class="cartao_conteudo_pergunta">
-                   <p> Saiba qual e a missao de nossa empresa! </p>
-                </div>
-
-                <div class="cartao_conteudo_resposta">
-                    <p>Nossa missao e... </p>
-                </div>
-            </div>
-    `
-    let respostaEstaVísivel- false;
-
-    function viraCartao () {
-        respostaEstaVísivel = IvariavelEstaVisível;
-        cartao.classList.toggle('active', viraCartao);
-
+        <h3>${item}</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>${resposta}</p>
+        </div>
+    </div>
     
+    `   
+   let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
-     
-    cartao.addEventListener('click', viraCartao);
-    
-    flashcard.appendChield(cartao);
-    
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
 
 }
