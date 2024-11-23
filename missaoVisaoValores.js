2361bfea4026ec0d18a1403970a9c2dab3cbba9f
anahorbatiuk5 <ana.vitoriahorbatiuk.ramos@escola.pr.gov.br>
2024-11-22 21:27:45 -0300

    10

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,22 +1,17 @@
 criaCartao (
     'Missão',
     'Saiba qual é a misasão de nossa empresa',
-    'missão é Encantar nossos clientes com doces artesanais
-'de alta qualidade, criados com paixão e ingredientes selecionados, proporcionando momentos únicos de alegria e sabor....'
+    'missão é Encantar nossos clientes com doces artesanais,de alta qualidade, criados com paixão e ingredientes selecionados, proporcionando momentos únicos de alegria e sabor....'
 )
 
 criaCartao (
     'Visão',
     'Saiba que é a visão de nossa empresa!',
-    'visão é Ser referência na arte da confeitaria, reconhecida
- 'por inovações saborosas e compromisso com
-'a qualidade e sustentabilidade, mantendo a tradição e o amor pelo que fazemos....'
+    'visão é Ser referência na arte da confeitaria, reconhecida por inovações saborosas e compromisso com,a qualidade e sustentabilidade, mantendo a tradição e o amor pelo que fazemos....'
 )
 
 criaCartao (
     'Valores',
     'Saiba quais são os valores de nossa empresa!',
-    'valores são Qualidade e inovação.
-'Sustentabilidade.
-'Comunidade e Alegria....'
+    'valores são Qualidade e inovação. Sustentabilidade. Comunidade e Alegria....'
 )
